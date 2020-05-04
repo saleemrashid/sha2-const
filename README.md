7c0b1bb78150c98fed13f484e01d15dfb1f4cdf3
Saleem Rashid <dev@saleemrashid.com>
2020-05-04 06:58:48 +0100

    Cargo.toml: Bump version

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # sha2-const
 
+[![Build status](https://github.com/saleemrashid/sha2-const/workflows/CI/badge.svg)](https://github.com/saleemrashid/sha2-const/actions)
+[![Crate](https://meritbadge.herokuapp.com/sha2-const)](https://crates.io/crates/sha2-const)
+[![Documentation](https://docs.rs/sha2-const/badge.svg)](https://docs.rs/sha2-const)
+
 `const fn` implementation of the SHA-2 family of hash functions.
 
 This crate allows you to use the SHA-2 hash functions as constant expressions
--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,7 @@
 /// Copies `n` bytes from `src` (starting at `src_offset`) to `dest` (starting
 /// at `dest_offset`).
 ///
-/// The source and destination must _not_ overlap. This functions exists because
+/// The source and destination must _not_ overlap. This function exists because
 /// subslices are not supported in `const fn`.
 #[inline(always)]
 pub(crate) const fn memcpy(
@@ -20,7 +20,7 @@ pub(crate) const fn memcpy(
 
 /// Sets `n` bytes in `dest` (starting at `dest_offset`) to `val`.
 ///
-/// This functions exists because subslices are not supported in `const fn`.
+/// This function exists because subslices are not supported in `const fn`.
 #[inline(always)]
 pub(crate) const fn memset(dest: &mut [u8], offset: usize, val: u8, n: usize) {
     let mut i = 0;
@@ -33,7 +33,7 @@ pub(crate) const fn memset(dest: &mut [u8], offset: usize, val: u8, n: usize) {
 /// Loads an unsigned 32-bit big endian integer from `src` (starting at
 /// `offset`).
 ///
-/// This functions exists because subslices are not supported in `const fn`.
+/// This function exists because subslices are not supported in `const fn`.
 #[inline(always)]
 pub(crate) const fn load_u32_be(src: &[u8], offset: usize) -> u32 {
     u32::from_be_bytes([
@@ -47,7 +47,7 @@ pub(crate) const fn load_u32_be(src: &[u8], offset: usize) -> u32 {
 /// Loads an unsigned 64-bit big endian integer from `src` (starting at
 /// `offset`).
 ///
-/// This functions exists because subslices are not supported in `const fn`.
+/// This function exists because subslices are not supported in `const fn`.
 #[inline(always)]
 pub(crate) const fn load_u64_be(src: &[u8], offset: usize) -> u64 {
     u64::from_be_bytes([
@@ -65,7 +65,7 @@ pub(crate) const fn load_u64_be(src: &[u8], offset: usize) -> u64 {
 /// Stores an unsigned 32-bit big endian integer into `dest` (starting at
 /// `offset`).
 ///
-/// This functions exists because subslices are not supported in `const fn`.
+/// This function exists because subslices are not supported in `const fn`.
 #[inline(always)]
 pub(crate) const fn store_u32_be(dest: &mut [u8], offset: usize, n: u32) {
     let bytes = u32::to_be_bytes(n);
@@ -75,7 +75,7 @@ pub(crate) const fn store_u32_be(dest: &mut [u8], offset: usize, n: u32) {
 /// Stores an unsigned 64-bit big endian integer into `dest` (starting at
 /// `offset`).
 ///
-/// This functions exists because subslices are not supported in `const fn`.
+/// This function exists because subslices are not supported in `const fn`.
 #[inline(always)]
 pub(crate) const fn store_u64_be(dest: &mut [u8], offset: usize, n: u64) {
     let bytes = u64::to_be_bytes(n);
@@ -85,7 +85,7 @@ pub(crate) const fn store_u64_be(dest: &mut [u8], offset: usize, n: u64) {
 /// Stores an unsigned 128-bit big endian integer into `dest` (starting at
 /// `offset`).
 ///
-/// This functions exists because subslices are not supported in `const fn`.
+/// This function exists because subslices are not supported in `const fn`.
 #[inline(always)]
 pub(crate) const fn store_u128_be(dest: &mut [u8], offset: usize, n: u128) {
     let bytes = u128::to_be_bytes(n);
