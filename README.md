69d0b0218ac126afa14157f06241282b17a5170d
Saleem Rashid <dev@saleemrashid.com>
2020-05-04 02:42:06 +0100

    README: Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# sha2-const
+
+`const fn` implementation of the SHA-2 family of hash functions.
+
+This crate allows you to use the SHA-2 hash functions as constant expressions
+in Rust. For all other usages, the [`sha2`] crate includes more optimized
+implementations of these hash functions.
+
+[`sha2`]: https://crates.io/crates/sha2
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 //! `const fn` implementation of the SHA-2 family of hash functions.
 //!
+//! This crate allows you to use the SHA-2 hash functions as constant
+//! expressions in Rust. For all other usages, the [`sha2`] crate includes more
+//! optimized implementations of these hash functions.
+//!
+//! [`sha2`]: https://crates.io/crates/sha2
+//!
 //! # Examples
 //!
 //! Compute the SHA-256 hash of the Bitcoin genesis block at compile time:
